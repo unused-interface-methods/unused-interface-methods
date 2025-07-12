f8f383d8981f8fb57e89e6dc83463a793b4067f3
comerc <andrew.kachanov@gmail.com>
2025-07-12 18:54:29 +0300

    Add TODO

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/unused-interface-methods/unused-interface-methods/internal/analizer"
 )
 
+// TODO: linter: The Idiom of Passing Interfaces and Returning Structures
+
 func main() {
 	for _, arg := range os.Args[1:] {
 		if arg == "-v" || arg == "--version" {
